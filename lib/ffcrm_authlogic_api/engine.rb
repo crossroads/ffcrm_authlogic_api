e16c90b4c52d4533f3b798be895baf821feebdaa
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-29 11:31:06 +0800

    Added Application Tokens tab

--- lib/ffcrm_authlogic_api/engine.rb
+++ lib/ffcrm_authlogic_api/engine.rb
@@ -1,6 +1,16 @@
 module FatFreeCRM
   module AuthlogicAPI
     class Engine < Rails::Engine
+      config.to_prepare do
+        begin
+          FatFreeCRM::Tabs.admin << {
+            :text => "Application Tokens",
+            :url => { :controller => "admin/application_accounts" }
+          }
+        rescue TypeError
+          puts "You must migrate your settings table."
+        end
+      end
     end
   end
 end
